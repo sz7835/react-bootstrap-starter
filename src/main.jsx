f5802a6743769a1dd24d723ce9e6c16233d1f80e
Steven Zavala <tocluda3229@gmail.com>
2025-07-13 19:14:03 -0400

    v1.3: updated main.jsx with cleaner root takeover logic

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css"; // Optional, for global tweaks
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Grab the real DOM element
+const rootElement = document.getElementById("root");
+
+// Create the React root manager
+const root = ReactDOM.createRoot(rootElement);
+
+// Inject the virtual DOM into the real DOM
+root.render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
